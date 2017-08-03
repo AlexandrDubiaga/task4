ea78287434731e6ef495a7807e2b323b7e813cd1
AlexandrDubiaga <alexandr.dubiaga@gmail.com>
2017-08-03 18:03:33 +0300

    Update PosgSql.php

--- PosgSql.php
+++ PosgSql.php
@@ -4,15 +4,15 @@ class PosgSql extends SQL
     private $user;
     private $password;
     private $host;
-    protected $dB;
+    protected $db;
     private $conn;
     public function __construct()
     {
         $this->host = 'localhost';
-        $this->dB = 'user1';
+        $this->db = 'user1';
         $this->user = 'user1';
         $this->password = 'user1z';
-        $this->conn = pg_connect("host=$host dbname=user1 user=$user password=$password");
+        $this->conn = pg_connect("host=$host dbname=$db user=$user password=$password");
         if (!$this->conn)
         {
             die("Could not open connection to database server");
