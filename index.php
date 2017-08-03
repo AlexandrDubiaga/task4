fce0c3827a04b4ab9f79435e494a3fc8e2618530
AlexandrDubiaga <alexandr.dubiaga@gmail.com>
2017-08-03 18:12:33 +0300

    Update index.php

--- index.php
+++ index.php
@@ -2,16 +2,36 @@
 include('Sql.php');
 include('Mysql.php');
 include('PosgSql.php');
-$conn = new Msql();
+
 
 $sql = new Sql();
+
+?>
+
+<h1>Postgres</h1><br>
+<?php
+$postgre = new PosgSql();
+$postGres = $postgre->select('data')->from('pg_test')->where('key','user10')->exec();
+$result = pg_query("$postGres");
+while($row = pg_fetch_assoc($result))
+{
+  echo $row['key']." ".$row['data']."<br>";
+}
+echo "<br>";
+echo "<br>";
+?>
+<h1>Mysql</h1><br>
+<?php
+$conn = new Msql();
 $connect = $conn->getConn();
-$str = $sql->select('data')->from('MY_TEST')->where('key','user10')->exec();
-$result = mysql_query("$str");
+$mysql = $conn->select('data')->from('MY_TEST')->where('key','user10')->exec();
+$result = mysql_query("$mysql");
 while($row = mysql_fetch_array($result))
 {
   echo $row['key']." ".$row['data']."<br>";
 }
-$postgre = new PosgSql();
+echo "<br>";
+echo "<br>";
+
 
 ?>
