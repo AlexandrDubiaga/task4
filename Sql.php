aa8dfb909f0ec9d319128eb085de8af1da3181fd
AlexandrDubiaga <alexandr.dubiaga@gmail.com>
2017-08-03 17:02:29 +0300

    Update Sql.php

--- Sql.php
+++ Sql.php
@@ -1,40 +1,63 @@
 <?php
-class Sql{
-    public $select;
-    public $from;
-    public $where;
-    public $table;
-    public $nameDB;
-    public $cons;
-
-    public function select($var){
-        $this->select = $var;
-        return $this;
-    }
-    public function table($var){
-        $this->table = $var;
-        return $this;
-    }
-    public function from($var){
-        $this->from = $var;
-        return $this;
-    }
-    public function nameDB($var){
-        $this->nameDB = $var;
-        return $this;
-    }
-    public function exec($var){
-       $this->cons = array();
-        $this->cons =  $this->select.' '.$this->table.' '.$this->from.' '.$this->nameDB;
-        return $this->cons;
-        }
-
-
-
-
+Class SQL
+{
+	private $sql;
+	private $selectVal;
+	private $fromVal;
+	private $whereVal;
+	private $insertVal;
+	private $valuesVal;
+	private $deleteVal;
+	private $updateVal;
+	private $setVal;
+	public function select($what)
+	{
+		if(!empty($what) && $what !== "*")
+		{
+			$this->selectVal = "Select `$what` ";
+			return $this;
+		}
+		else
+		{
+			return ERROR_VAL;
+		}
+	}
+	
+	public function from($table)
+	{
+		if(!empty($table))
+		{
+			$this->fromVal = "from `$table` ";
+			return $this;
+		}
+		else
+		{
+			return ERROR_VAL;
+		}
+	}
+	
+	public function where($key, $values)
+	{
+		if(!empty($key) && !empty($values))
+		{
+			
+			$this->whereVal = "where `$key` = '$values' ";
+			return $this;
+		}
+		else
+		{
+			return ERROR_VAL;
+		}
+	}
+	
+	public function exec()
+	{
+		$this->sql= $this->selectVal . $this->fromVal . $this->whereVal;
+		return $this->sql;
+	}
+	
 
 }
-
 
 
 
