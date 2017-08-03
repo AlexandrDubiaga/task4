88921bd49d02875e8f053dd5557dfe80e7427030
AlexandrDubiaga <alexandr.dubiaga@gmail.com>
2017-08-03 17:49:11 +0300

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ include('Mysql.php');
 $conn = new Msql();
 $sql = new Sql();
 $connect = $conn->getConn();
-$str = $sql->select('data')->from('MY_TEST')->where('key','user2')->exec();
+$str = $sql->select('data')->from('MY_TEST')->where('key','user10')->exec();
 $result = mysql_query("$str");
 while($row = mysql_fetch_array($result))
 {
