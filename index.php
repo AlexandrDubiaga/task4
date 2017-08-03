8a3e8e5672e48152edf802f359462b7c3f7ba30d
AlexandrDubiaga <alexandr.dubiaga@gmail.com>
2017-08-03 19:42:24 +0300

    Update index.php

--- index.php
+++ index.php
@@ -21,9 +21,8 @@ echo "<br>";
 <?php
 $conn = new Msql();
 $connect = $conn->getConn();
-$filds = array('key','data');
 $value = array('Hosting','Company');
-$mysql = $conn->insert('MY_TEST',$filds)->values($value)->exec("INSERT");
+$mysql = $conn->insert('MY_TEST')->values($value)->exec("INSERT");
 var_dump($mysql);
 /*$result = mysql_query("$mysql");
 while($row = mysql_fetch_array($result,MYSQL_ASSOC))
