3fbf7177b7b2e6f9ed4fda9c133e45c5e502b07e
AlexandrDubiaga <alexandr.dubiaga@gmail.com>
2017-08-03 17:20:16 +0300

    Update index.php

--- index.php
+++ index.php
@@ -3,10 +3,6 @@ include('Sql.php');
 include('Mysql.php');
 $conn = new Msql();
 $sql = new Sql();
-$x = $sql->select('SELECT')->table('nameSVT')->from('FROM')->nameDB('svt')->exec();
-$result = $conn->getConn()->query($x);
-while($row = mysqli_fetch_array($result)){
-    echo $row['nameSVT']."<br>";
-}
+
 
 ?>
