ed0fc6007dcd70ac4ecc2e2a7acb77eec86de337
AlexandrDubiaga <alexandr.dubiaga@gmail.com>
2017-08-04 20:05:00 +0300

    Update index.php

--- index.php
+++ index.php
@@ -22,9 +22,9 @@ echo "<br>";
 <?php
 $conn = new Msql();
 $connect = $conn->getConn();
-/*$value = array('alex','cardo');
+$value = array('alex','cardo');
 $mysql = $conn->insert('MY_TEST')->values($value)->exec("INSERT");
-mysql_query("$mysql");*/
+mysql_query("$mysql");
 
 echo "<br>";
 echo "<br>";
